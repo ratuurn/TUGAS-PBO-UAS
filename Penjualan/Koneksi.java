88b3f8dbaaef52166aef4dab0478e2ab2b334d66
ratuurn <114390295+ratuurn@users.noreply.github.com>
2023-01-31 00:07:02 -0800

    Create Koneksi.java

---
+++ Penjualan/Koneksi.java
@@ -0,0 +1,31 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ */
+public class Koneksi {
+    private static Connection koneksi;
+    
+    public static Connection getKoneksi(){
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/penjualanku";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Berhasil");
+            } catch (SQLException e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String args[]){
+        getKoneksi();
+    }  
+}
